0cc8cafb53601c939cfc01c651f423376537201a
Javier Casal Ruiz <javier.casal@flywire.com>
2024-02-24 20:32:57 +0100

    spawn enemies

--- game.go
+++ game.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -8,22 +10,33 @@ const (
 	ScreenWidth       = 800
 	ScreenHeight      = 600
 	SpriteScaleFactor = 4
+	enemySpawnTime    = 500 * time.Millisecond
 )
 
 func NewGame() *Game {
 	return &Game{
-		player: NewPlayer(),
-		world:  NewWorld(),
+		player:          NewPlayer(),
+		world:           NewWorld(),
+		enemySpawnTimer: NewTimer(enemySpawnTime),
 	}
 }
 
 type Game struct {
-	player *Player
-	world  *World
+	player          *Player
+	world           *World
+	enemies         []*Enemy
+	enemySpawnTimer *Timer
 }
 
 func (g *Game) Update() error {
 	g.player.Update()
+	g.enemySpawnTimer.Update()
+	if g.enemySpawnTimer.IsReady() {
+		g.enemySpawnTimer.Reset()
+
+		m := NewEnemy()
+		g.enemies = append(g.enemies, m)
+	}
 
 	return nil
 }
